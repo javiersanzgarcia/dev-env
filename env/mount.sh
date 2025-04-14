0db3fe4797504a0bbfc9128e90805eb9750ac27b
Javier Sanz García <pracker@gmail.com>
2025-04-14 14:05:22 +0200

    feat: mount script Archivo witjout umask

--- env/mount.sh
+++ env/mount.sh
@@ -4,4 +4,4 @@ echo " "
 echo "#### Mounting exFAT partition"
 echo " "
 
-sudo mount -o uid=1000,gid=1000,umask=0777 -t exfat UUID="4870-BE1C" /media/pracker/Archivo
+sudo mount -o uid=1000,gid=1000,umask=0000 -t exfat UUID="4870-BE1C" /media/pracker/Archivo
